2fb510285cf73d5b9f890f4f57ec8d0f0524ca4c
Rob Dickerson <rob@robd.io>
2021-04-23 16:54:50 -0400

    Houdini WIP

--- src/Ceili/InvariantInference/Houdini.hs
+++ src/Ceili/InvariantInference/Houdini.hs
@@ -1,11 +1,10 @@
 module Ceili.InvariantInference.Houdini
-  (
+  ( infer
   ) where
 
 import Ceili.Assertion ( Assertion(..) )
 import Ceili.InvariantInference.LinearInequalities
 import Ceili.Name ( TypedName )
-import Ceili.PTS.ForwardPT
 import qualified Ceili.SMT as SMT
 import Control.Concurrent.Timeout ( timeout )
 import Control.Monad ( filterM )
@@ -13,28 +12,41 @@ import Control.Monad.IO.Class ( liftIO )
 import Data.Set ( Set )
 import qualified Data.Set as Set
 
-infer :: ForwardPT p -> Assertion -> Assertion
-infer forwardPT precond = ATrue
+infer :: Set TypedName -> Set Integer -> Int -> Assertion -> (Assertion -> IO Assertion) -> IO Assertion
+infer names lits size precond computeSP = do
+  candidates <- findCandidates names lits size precond
+  inductive  <- houdini candidates computeSP
+  return $ And inductive
 
 findCandidates :: Set TypedName
                -> Set Integer
                -> Int
                -> Assertion
-               -> IO (Set Assertion)
-findCandidates names lits depth precond = do
-  let candidates = linearInequalities names lits depth
-  filtered <- filterM (checkValid . Imp precond) $ Set.toList candidates
-  return $ Set.fromList filtered
+               -> IO [Assertion]
+findCandidates names lits size precond = do
+  let candidates = linearInequalities names lits size
+  filterM (checkValid . Imp precond) $ Set.toList candidates
 
--- TODO: These belong in some kind of SMT monad.
+houdini :: [Assertion]
+        -> (Assertion -> IO Assertion)
+        -> IO [Assertion]
+houdini candidates computeSP = do
+  hLog $ "Starting houdini pass with " ++ (show $ length candidates) ++ " candidate clauses."
+  sp <- computeSP $ And $ candidates
+  inductive <- filterM (checkValid . Imp sp) candidates
+  if (length inductive == length candidates)
+    then return candidates
+    else houdini inductive computeSP
+
+-- TODO: These belong in some kind of config environment.
 withTimeout t = liftIO $ timeout (1000000 * 2) t
-smtLog = putStrLn
+hLog = putStrLn
 
 checkValid :: Assertion -> IO Bool
 checkValid assertion = do
   result <- withTimeout $ SMT.checkValid assertion
   case result of
-    Nothing               -> do smtLog "SMT timeout"; return False
+    Nothing               -> do hLog "SMT timeout"; return False
     Just SMT.Valid        -> return True
     Just (SMT.Invalid _)  -> return False
-    Just SMT.ValidUnknown -> do smtLog "SMT unknown"; return False
+    Just SMT.ValidUnknown -> do hLog "SMT unknown"; return False
