e3b66a8de9fd7380d36d2a318b2197dca859627e
Rob Dickerson <rob@robd.io>
2021-04-24 15:13:52 -0400

    Adjusting Ceili exports.

--- src/Ceili.hs
+++ src/Ceili.hs
@@ -1,3 +1,13 @@
 module Ceili
-  (
+  ( Ceili
+  , Env(..)
+  , Name(..)
+  , Type(..)
+  , TypedName(..)
+  , runCeili
+  , showSMT
   ) where
+
+import Ceili.CeiliEnv
+import Ceili.Name
+import Ceili.SMTString
