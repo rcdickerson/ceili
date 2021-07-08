210825bc0381c4328c112b8266bc536067fd4ce6
Rob Dickerson <rob@robd.io>
2021-07-08 12:52:29 -0400

    Adding FunImp PTS tests for nested fun calls.

--- test/Ceili/Language/FunImpTest.hs
+++ test/Ceili/Language/FunImpTest.hs
@@ -21,24 +21,50 @@ add1Impl = FunImpl { fimpl_params = [x]
                    , fimpl_returns = [y]
                    }
 
+add2Impl :: FunImpl
+add2Impl = FunImpl { fimpl_params = [x]
+                   , fimpl_body = impSeq
+                                  [ impCall "add1" [AVar x] [y]
+                                  , impAsgn y $ AAdd (AVar y) (ALit 1)
+                                  ]
+                   , fimpl_returns = [y]
+                   }
+
+implEnv = Map.fromList [ ("add1", add1Impl)
+                       , ("add2", add2Impl)
+                       ]
+
 prog1 :: FunImpProgram
 prog1 = impSeq [ impCall "add1" [AVar x] [x]
                , impAsgn x $ AAdd (AVar x) (ALit 1)
                ]
 
+prog2 :: FunImpProgram
+prog2 = impSeq [ impCall "add2" [AVar x] [x]
+               , impAsgn x $ AAdd (AVar x) (ALit 1)
+               ]
+
 
 test_backwardPT = do
-  let impls = Map.fromList [("add1", add1Impl)]
-  let expected = Eq (Var $ TypedName x Int) (Num 0)
-  actualEither <- runCeili (mkEnv prog1) $ impBackwardPT impls prog1 $ Eq (Var $ TypedName x Int) (Num 2)
-  case actualEither of
+  result <- runCeili (mkEnv prog1) $ impBackwardPT implEnv prog1 $ Eq (Var $ TypedName x Int) (Num 2)
+  case result of
     Left err     -> assertFailure $ show err
-    Right actual -> assertImplies actual expected
+    Right actual -> assertImplies actual $ Eq (Var $ TypedName x Int) (Num 0)
+
+test_backwardPTNested = do
+  result <- runCeili (mkEnv prog2) $ impBackwardPT implEnv prog2 $ Eq (Var $ TypedName x Int) (Num 3)
+  case result of
+    Left err     -> assertFailure $ show err
+    Right actual -> assertImplies actual $ Eq (Var $ TypedName x Int) (Num 0)
 
 test_forwardPT = do
-  let impls = Map.fromList [("add1", add1Impl)]
-  let expected = Eq (Var $ TypedName x Int) (Num 2)
-  actualEither <- runCeili (mkEnv prog1) $ impForwardPT impls prog1 $ Eq (Var $ TypedName x Int) (Num 0)
-  case actualEither of
+  result <- runCeili (mkEnv prog1) $ impForwardPT implEnv prog1 $ Eq (Var $ TypedName x Int) (Num 0)
+  case result of
     Left err     -> assertFailure $ show err
-    Right actual -> assertImplies actual expected
+    Right actual -> assertImplies actual $ Eq (Var $ TypedName x Int) (Num 2)
+
+test_forwardPTNested = do
+  result <- runCeili (mkEnv prog2) $ impForwardPT implEnv prog2 $ Eq (Var $ TypedName x Int) (Num 0)
+  case result of
+    Left err     -> assertFailure $ show err
+    Right actual -> assertImplies actual $ Eq (Var $ TypedName x Int) (Num 3)
